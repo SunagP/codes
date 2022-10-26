35059c8a1b91f05dc4ff31518784023e6050b10c
SunagP <sunag.p3007@gmail.com>
2022-10-26 14:28:27 +0530

    new updates

--- javacodes/try1.java
+++ javacodes/try1.java
@@ -1,10 +1,17 @@
-// package javacodes;
+// import javax.script.ScriptEngineManager;
+// import javax.script.ScriptEngine;
+// import javax.script.ScriptException;
 
-public class try1 {
-    public static void main(String[] args) {  
-        //Code of Java for loop  
-        for(int i=1;i<=10;i++){  
-            System.out.println(i);  
-        }  
-    }  
-}
+// public class try1 {
+//   public static void main(String[] args) throws ScriptException {
+//     ScriptEngineManager mgr = new ScriptEngineManager();
+//     ScriptEngine engine = mgr.getEngineByName("JavaScript");
+//     String foo = "40+(2-6)/2*3";
+//     System.out.println(engine.eval(foo));
+//     } 
+// }
+
+/* A Java program to evaluate a
+given expression where tokens
+are separated by space.
+*/
